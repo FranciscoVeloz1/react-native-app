504084ea9b834b702044a049181bf72282242284
Francisco Veloz <franciscoveloz245@gmail.com>
2021-06-12 21:09:27 -0500

    Iniciamos nuestra primera app en React-Native, quedamos en el inicio de botones

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native'
+// import ball from './assets/ball.png'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World</Text >
+      <Image
+        source={{uri: 'https://picsum.photos/200/200'}}
+        // source={ball}
+        style={styles.image}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#292929'
   },
-});
+
+  title: {
+    fontSize: 30,
+    color: '#fff'
+  },
+
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100
+  }
+})
+
+export default App;
